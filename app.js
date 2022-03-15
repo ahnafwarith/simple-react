906a6c983c9c39289551bf83bf28de34f8c01fa9
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-03-16 05:19:55 +0600

    added

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("Hello World")
+const loadCountries = () => {
+    fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => displayCountries(data))
+}
+
+const displayCountries = countries => {
+    console.log(countries);
+    const allCountriesHTML = countries.map(country => getCountryHTML(country))
+    console.log(allCountriesHTML)
+    /* Dynamically displaying the data from the API */
+    document.getElementById('container').innerHTML = allCountriesHTML.join(" ")
+}
+
+const getCountryHTML = country => {
+    console.log(country)
+    return `
+        <div>
+            <h2>${country.name.common}</h2>
+            <img src="${country.flags.png}">
+        </div>
+    `
+}
+
+loadCountries();
