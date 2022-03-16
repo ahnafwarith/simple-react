f0e82e72f37d395bf60712dc6bed3e138bea76c3
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-03-16 06:26:45 +0600

    learned how to store values directly from object using destructuring into multiple variables

--- app.js
+++ app.js
@@ -5,19 +5,19 @@ const loadCountries = () => {
 }
 
 const displayCountries = countries => {
-    console.log(countries);
+    /*   console.log(countries); */
     const allCountriesHTML = countries.map(country => getCountryHTML(country))
-    console.log(allCountriesHTML)
+    /*     console.log(allCountriesHTML) */
     /* Dynamically displaying the data from the API */
     document.getElementById('container').innerHTML = allCountriesHTML.join(" ")
 }
 
 const getCountryHTML = country => {
-    console.log(country)
+    const { name, flags } = country
     return `
         <div>
-            <h2>${country.name.common}</h2>
-            <img src="${country.flags.png}">
+            <h2>${name.common}</h2>
+            <img src="${flags.png}">
         </div>
     `
 }
